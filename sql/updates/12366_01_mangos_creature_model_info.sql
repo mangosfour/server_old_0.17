3203a716ea88419670c022886c4f9ab33b2455fe
X-Savior <xsavior@acid.com>
2013-02-09 12:46:32 +0100

    [12366] Add missing Goblin and Worg ModelId Data

---
+++ sql/updates/12366_01_mangos_creature_model_info.sql
@@ -0,0 +1,5 @@
+INSERT INTO creature_model_info (modelid, bounding_radius, combat_reach, gender, modelid_other_gender, modelid_alternative) VALUES
+(6894,0.389,1.5,0,6895,0),
+(6895,0.389,1.5,1,6894,0),
+(29422,0.389,1.5,0,29423,0),
+(29423,0.389,1.5,1,29422,0);
