6a2feccf72418f80ba5925dddb5256824147838f
faramir118 <faramir118@gmail.com>
2013-02-23 12:38:25 +0100

    [c12503] fix clang compile error from implicit type conversion
    (based on commit [12366] - ce05512581df2eec6687a76bcfc4415dc72bdb23)

--- src/game/Unit.h
+++ src/game/Unit.h
@@ -2091,7 +2091,7 @@ void Unit::CallForAllControlledUnits(Func const& func, uint32 controlledMask)
             func(pet);
 
     if (controlledMask & CONTROLLED_MINIPET)
-        if (Unit* mini = GetMiniPet())
+        if (Pet* mini = GetMiniPet())
             func(mini);
 
     if (controlledMask & CONTROLLED_GUARDIANS)
@@ -2122,7 +2122,7 @@ bool Unit::CheckAllControlledUnits(Func const& func, uint32 controlledMask) cons
                 return true;
 
     if (controlledMask & CONTROLLED_MINIPET)
-        if (Unit const* mini = GetMiniPet())
+        if (Pet const* mini = GetMiniPet())
             if (func(mini))
                 return true;
 
