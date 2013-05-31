a974f53ab909bf4899f520272b6d0ce9b2be83b6
Xfurry <xfurry@scriptdev2.com>
2013-05-31 11:07:19 +0100

    [c12598] Implement spells 19869 and 20037

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -1061,6 +1061,22 @@ void Spell::EffectDummy(SpellEffectEntry const* effect)
 
                     return;
                 }
+                case 19869:                                 // Dragon Orb
+                {
+                    if (!unitTarget || unitTarget->GetTypeId() != TYPEID_PLAYER || unitTarget->HasAura(23958))
+                        return;
+
+                    unitTarget->CastSpell(unitTarget, 19832, true);
+                    return;
+                }
+                case 20037:                                 // Explode Orb Effect
+                {
+                    if (!unitTarget)
+                        return;
+
+                    unitTarget->CastSpell(unitTarget, 20038, true);
+                    return;
+                }
                 case 20577:                                 // Cannibalize
                 {
                     if (unitTarget)
