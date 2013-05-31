cadb99f768017cbb820e0d729374e8d51394d8a8
Xfurry <xfurry@scriptdev2.com>
2013-05-31 10:51:44 +0100

    [c12582] Add support for server-side spells attr, attr_ex, attr_ex2 and effect0_target_b columns

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
  #define REVISION_DB_CHARACTERS "required_12447_02_characters_calendar_invites"
- #define REVISION_DB_MANGOS "required_c12564_01_mangos_spell_template"
+ #define REVISION_DB_MANGOS "required_c12594_01_mangos_spell_template"
  #define REVISION_DB_REALMD "required_c12484_02_realmd_account_access"
 #endif // __REVISION_SQL_H__
