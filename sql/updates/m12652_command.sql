0eb1a56b95533d7c82537ccac07d494b72da0906
sanctum32 <andriuspel@gmail.com>
2013-07-27 16:55:06 +0300

    [m12655] updated right revisions... Added missing mangos.sql change for last commit

--- sql/updates/m12652_command.sql
+++ sql/updates/m12652_command.sql
@@ -1,4 +1,4 @@
 update command set `name`='summon' where `name`='namego';
 update command set `name`='appear' where `name`='goname';
 
-ALTER TABLE db_version CHANGE COLUMN required_c12631_01_mangos_creature required_m12652_command bit;
+ALTER TABLE db_version CHANGE COLUMN required_c12631_01_mangos_creature required_m12654_command bit;
--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
  #define REVISION_DB_CHARACTERS "required_c12631_01_characters_corpse"
- #define REVISION_DB_MANGOS "required_m12652_command"
+ #define REVISION_DB_MANGOS "required_m12654_command"
  #define REVISION_DB_REALMD "required_c12484_02_realmd_account_access"
 #endif // __REVISION_SQL_H__
