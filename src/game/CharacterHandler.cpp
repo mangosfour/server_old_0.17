0852769792d4a52745c0e17d1aae39a60b7e6358
zamalaev <zamalaev88@mail.ru>
2014-05-12 17:47:50 +0400

    CMSG_PLAYER_LOGIN 5.4.7 18019

--- src/game/CharacterHandler.cpp
+++ src/game/CharacterHandler.cpp
@@ -559,13 +559,29 @@ void WorldSession::HandleCharDeleteOpcode(WorldPacket& recv_data)
     SendPacket(&data);
 }
 
-void WorldSession::HandlePlayerLoginOpcode(WorldPacket& recv_data)
+void WorldSession::HandlePlayerLoginOpcode(WorldPacket& recvData)
 {
     ObjectGuid playerGuid;
 
-    recv_data.ReadGuidMask<1, 5, 0, 2, 7, 6, 3, 4>(playerGuid);
-    recv_data.ReadGuidBytes<6, 4, 3, 5, 0, 2, 7, 1>(playerGuid);
-    float unk = recv_data.ReadSingle();
+    playerGuid[7] = recvData.ReadBit();
+    playerGuid[6] = recvData.ReadBit();
+    playerGuid[0] = recvData.ReadBit();
+    playerGuid[4] = recvData.ReadBit();
+    playerGuid[5] = recvData.ReadBit();
+    playerGuid[2] = recvData.ReadBit();
+    playerGuid[3] = recvData.ReadBit();
+    playerGuid[1] = recvData.ReadBit();
+
+    recvData.ReadByteSeq(playerGuid[5]);
+    recvData.ReadByteSeq(playerGuid[0]);
+    recvData.ReadByteSeq(playerGuid[1]);
+    recvData.ReadByteSeq(playerGuid[6]);
+    recvData.ReadByteSeq(playerGuid[7]);
+    recvData.ReadByteSeq(playerGuid[2]);
+    recvData.ReadByteSeq(playerGuid[3]);
+    recvData.ReadByteSeq(playerGuid[4]);
+
+    float unk = recvData.ReadSingle();
 
     DEBUG_LOG("WORLD: Received opcode Player Logon Message from %s, unk float: %f", playerGuid.GetString().c_str(), unk);
     if (PlayerLoading() || GetPlayer() != NULL)
--- src/game/Opcodes.h
+++ src/game/Opcodes.h
@@ -521,7 +521,7 @@ enum Opcodes
 	CMSG_PLAY_DANCE                                       = 0x144C,
 	CMSG_PLAYED_TIME                                      = 0x0804, // 4.3.4 15595
 	CMSG_PLAYER_AI_CHEAT                                  = 0x126D,
-	CMSG_PLAYER_LOGIN                                     = 0x01E1, // 5.4.1 17538
+	CMSG_PLAYER_LOGIN                                     = 0x17D3, // 5.4.7 18019
 	CMSG_PLAYER_LOGOUT                                    = 0x104B,
 	CMSG_PROFILEDATA_REQUEST                              = 0x14CA,
 	CMSG_PUSHQUESTTOPARTY                                 = 0x4B14, // 4.3.4 15595
