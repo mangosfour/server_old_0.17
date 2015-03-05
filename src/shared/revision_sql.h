80493f61970f94ed37399818ad170370eb1067c3
zamalaev <zamalaev88@mail.ru>
2015-03-05 20:40:51 +0300

    Some fixed worldserver start, update DBC structur 5.4.8

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -25,6 +25,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
  #define REVISION_DB_CHARACTERS "required_12712_01_characters_characters"
- #define REVISION_DB_MANGOS "required_12741_01_mangos"
+ #define REVISION_DB_MANGOS "required_12752_01_mangos_reputation_spillover_template"
 #define REVISION_DB_REALMD "required_20140607_Realm_Resync"
 #endif // __REVISION_SQL_H__
